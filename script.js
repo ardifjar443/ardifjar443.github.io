6814f403ed67ecb767664522163fc7879b433d53
ardi <ardifjar443>
2022-11-21 20:12:24 +0700

    ehe

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-function myFunction() {
-    var x = document.getElementById("text-content");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-    }
+const scriptURL =
+    'https://script.google.com/macros/s/AKfycbyyzFTcUzBWdEswUiiLJIT15F_MbX5At44Wc4ZN0LzriKyuixjtiu9uCChQrpcU7vOb/exec';
+const form = document.forms['form'];
+const btnkirim = document.querySelector('.btn-kirim');
+const btnloading = document.querySelector('.btn-loading');
+let popup = document.getElementById("popup");
+let popup2 = document.getElementById("popup2");
 
-    var y = document.getElementById("content-down");
-    if (y.style.display === "inline-block") {
-        y.style.display = "none";
-    } else {
-        y.style.display = "inline-block";
-    }
 
-    var i = document.getElementById("content-up");
-    if (i.style.display === "none") {
-        i.style.display = "inline-block";
-    } else {
-        i.style.display = "none";
-    }
+function openPopup() {
+    popup.classList.add("open-popup")
 }
+
+function closePopup() {
+    popup.classList.remove("open-popup")
+}
+
+function closePopup2() {
+    popup2.classList.remove("open-popup2")
+}
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    // klik
+    // loading
+    btnloading.classList.toggle('d-none');
+    btnkirim.classList.toggle('d-none');
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            btnloading.classList.toggle('d-none');
+            btnkirim.classList.toggle('d-none');
+            form.reset();
+            console.log('Success!', response)
+            popup.classList.remove("open-popup")
+            popup2.classList.add("open-popup2")
+        })
+        .catch(error => console.error('Error!', error.mesimage.pngsage))
+})
