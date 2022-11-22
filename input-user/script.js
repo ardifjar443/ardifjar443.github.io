62b53ad3a18214942dcdc3327678bfb4df0de902
ardi <ardifjar443>
2022-11-22 18:07:35 +0700

    cape

--- input-user/script.js
+++ input-user/script.js
@@ -1,42 +1,52 @@
-const scriptURL2 =
+const scriptURL =
     'https://script.google.com/macros/s/AKfycbwaCi0prWnW8qFTYjbgD9-8JzDBuPk2mWWGsPYiWAjrh1XsOFVd_2NaDORwXwtD0AGa/exec';
-const form2 = document.forms['form2'];
-const btnkirim2 = document.querySelector('.btn-kirim2');
-const btnloading2 = document.querySelector('.btn-loading2');
-let popupkontak = document.getElementById("popupkontak");
-let popupthanks = document.getElementById("popupthanks");
+const form = document.forms['form'];
+const btnkirim = document.querySelector('.btn-kirim');
+const btnloading = document.querySelector('.btn-loading');
+let popup2 = document.getElementById("popup2");
+let popup3 = document.getElementById("popup3");
 
+// // navbar
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarSupportedContent')
+// const bsCollapse = new bootstrap.Collapse(menuToggle)
+// navLinks.forEach((l) => {
+//     l.addEventListener('click', () => {
+//         bsCollapse.toggle()
+//     })
+// })
+// // end navbar
 
-function openPopupKontak() {
-    popupkontak.classList.add("open-popupkontak")
+function openPopup() {
+    popup2.classList.add("open-popup2")
 }
 
-function closePopupKontak() {
-    popupkontak.classList.remove("open-popupkontak")
+function closePopup() {
+    popup2.classList.remove("open-popup2")
 }
 
-function closePopupThanks() {
-    popupthanks.classList.remove("open-popupthanks")
+function closePopup2() {
+    popup3.classList.remove("open-popup3")
 }
 
 
-form2.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
     e.preventDefault()
     // klik
     // loading
-    btnloading2.classList.toggle('d-none');
-    btnkirim2.classList.toggle('d-none');
-    fetch(scriptURL2, {
+    btnloading.classList.toggle('d-none');
+    btnkirim.classList.toggle('d-none');
+    fetch(scriptURL, {
             method: 'POST',
-            body: new FormData(form2)
+            body: new FormData(form)
         })
         .then(response => {
-            btnloading2.classList.toggle('d-none');
-            btnkirim2.classList.toggle('d-none');
-            form2.reset();
+            btnloading.classList.toggle('d-none');
+            btnkirim.classList.toggle('d-none');
+            form.reset();
             console.log('Success!', response)
-            popupkontak.classList.remove("open-popupkontak")
-            popupthanks.classList.add("open-popupthanks")
+            popup2.classList.remove("open-popup2")
+            popup3.classList.add("open-popup3")
         })
         .catch(error => console.error('Error!', error.mesimage.pngsage))
 })
