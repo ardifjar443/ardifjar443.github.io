37513cdbddd522fd6d58be7c3f74030aa4cb7966
ardifjar443 <ardifjar443@gmail.com>
2022-12-29 17:43:40 +0700

    ehe

---
+++ login/script.js
@@ -0,0 +1,86 @@
+var current = null;
+document.querySelector('#email').addEventListener('focus', function (e) {
+    if (current) current.pause();
+    current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+            value: 0,
+            duration: 700,
+            easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+            value: '240 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+        }
+    });
+});
+document.querySelector('#password').addEventListener('focus', function (e) {
+    if (current) current.pause();
+    current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+            value: -336,
+            duration: 700,
+            easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+            value: '240 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+        }
+    });
+});
+document.querySelector('#submit').addEventListener('focus', function (e) {
+    if (current) current.pause();
+    current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+            value: -730,
+            duration: 700,
+            easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+            value: '530 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+        }
+    });
+});
+
+// Login system
+function Login() {
+    var user = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+
+    if (user == "ardifjar443@gmail.com" && pass == "ratuayunurfajar") {
+        window.location.href = "./isi"
+    } else if (user == "ratua4820@gmail.com" && pass == "ardifajararifin") {
+        window.location.href = "./isi"
+    } else {
+        alert("username dan password anda salah")
+    }
+}
+
+// eye
+const root = document.documentElement;
+const eye = document.getElementById('eyeball');
+const beam = document.getElementById('beam');
+const passwordInput = document.getElementById('password');
+
+root.addEventListener('mousemove', (e) => {
+    let rect = beam.getBoundingClientRect();
+    let mouseX = rect.right + (rect.width / 2);
+    let mouseY = rect.top + (rect.height / 2);
+    let rad = Math.atan2(mouseX - e.pageX, mouseY - e.pageY);
+    let degrees = (rad * (20 / Math.PI) * -1) - 350;
+
+    root.style.setProperty('--beamDegrees', `${degrees}deg`);
+});
+
+eye.addEventListener('click', e => {
+    e.preventDefault();
+    document.body.classList.toggle('show-password');
+    passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password'
+    passwordInput.focus();
+});
