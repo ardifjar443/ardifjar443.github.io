9de3d5b38ccb737442a52ea6678858bd3bb09ada
ardi <ardifjar443@gmail.com>
2023-10-01 15:23:00 +0700

    update: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -93,6 +93,13 @@ const Footer = ({ activeTab, setActiveTab }) => {
                     >
                       <i className="fab fa-github"></i>
                     </a>
+                    <a
+                      href="https://www.linkedin.com/in/ardi-fajar-arifin/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fab fa-linkedin"></i>
+                    </a>
                   </div>
                 </div>
               </div>
